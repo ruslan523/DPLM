ec2701a8ab37bbc3efa7a3820677fa14544e9a89
ruslan523 <rh523@yandex.ru>
2018-06-14 22:20:42 +0300

    4 графика
    Поддержка 4 графиков. Отображение стало красивее засчёт уменьшения высоты. Проблема с "уплывающим" последним элементом осталась

--- DPLM/Form1.cs
+++ DPLM/Form1.cs
@@ -18,27 +18,21 @@ namespace DPLM
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        void graph(Pen pB, Pen pR, StreamReader sr)
         {
             Graphics g = Graphics.FromHwnd(pictureBox1.Handle);
-            Pen pB = new Pen(Color.Blue, 1);
-            Pen pR = new Pen(Color.Red, 3);
-            string path = @"data1.txt";
             int counter = 0;
             int[] mass = new int[1000];
-            using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
+            string line;
+            while ((line = sr.ReadLine()) != null)
             {
-                string line;
-                while ((line = sr.ReadLine()) != null)
-                {
-                    mass[counter] = Convert.ToInt32(line);
-                    counter++;
-                }
+                mass[counter] = Convert.ToInt32(line);
+                counter++;
             }
             for (int i = 0; i < counter; i++)
             {
                 label1.Text += " " + Convert.ToString(mass[i]);
-                mass[i] = 400 - mass[i] * 100 + 100;
+                mass[i] = 400 - mass[i] * 20 + 100;
             }
             PointF[] pnts = new PointF[1000];
             for (int i = 0; i < counter; i++)
@@ -50,6 +44,43 @@ namespace DPLM
                 g.DrawEllipse(pR, i * 10, mass[i], 4, 4);
             }
             g.DrawCurve(pB, pnts);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics g = Graphics.FromHwnd(pictureBox1.Handle);
+            Pen[] pB = new Pen[]
+            {
+                new Pen(Color.DarkBlue, 1),
+                new Pen(Color.DarkRed, 1),
+                new Pen(Color.DarkOrange, 1),
+                new Pen(Color.DarkGreen, 1)
+            };
+            Pen[] pR = new Pen[]
+{
+                new Pen(Color.Blue, 1),
+                new Pen(Color.Red, 1),
+                new Pen(Color.Orange, 1),
+                new Pen(Color.Green, 1)
+};
+            string[] path = new string[]
+            {
+                @"data1.txt",
+                @"data2.txt",
+                @"data3.txt",
+                @"data4.txt"
+            };
+            StreamReader[] sr = new StreamReader[]
+            {
+                new StreamReader(path[0], System.Text.Encoding.Default),
+                new StreamReader(path[1], System.Text.Encoding.Default),
+                new StreamReader(path[2], System.Text.Encoding.Default),
+                new StreamReader(path[3], System.Text.Encoding.Default)
+            };
+            graph(pB[0], pR[0], sr[0]);
+            graph(pB[1], pR[1], sr[1]);
+            graph(pB[2], pR[2], sr[2]);
+            graph(pB[3], pR[3], sr[3]);
         }
 
         private void label1_Click(object sender, EventArgs e)
