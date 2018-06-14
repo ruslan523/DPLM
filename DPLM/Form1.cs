670edbe2661319190252dfa30a3f37f88fc9ed53
ruslan523 <rh523@yandex.ru>
2018-06-14 21:46:45 +0300

    Работающий один график
    Работающий один график. Проблема с конечным элементом

---
+++ DPLM/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DPLM
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics g = Graphics.FromHwnd(pictureBox1.Handle);
+            Pen pB = new Pen(Color.Blue, 1);
+            Pen pR = new Pen(Color.Red, 3);
+            string path = @"data1.txt";
+            int counter = 0;
+            int[] mass = new int[1000];
+            using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    mass[counter] = Convert.ToInt32(line);
+                    counter++;
+                }
+            }
+            for (int i = 0; i < counter; i++)
+            {
+                label1.Text += " " + Convert.ToString(mass[i]);
+                mass[i] = 400 - mass[i] * 100 + 100;
+            }
+            PointF[] pnts = new PointF[1000];
+            for (int i = 0; i < counter; i++)
+            {
+                pnts[i] = new PointF(2 + i * 10, mass[i]);
+            }
+            for (int i = 0; i < counter; i++)
+            {
+                g.DrawEllipse(pR, i * 10, mass[i], 4, 4);
+            }
+            g.DrawCurve(pB, pnts);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ DPLM/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("DPLM")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DPLM")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("c51c63b2-8f4d-4b6e-a3c9-0d9c2d17d033")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
